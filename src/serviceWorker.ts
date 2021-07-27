94b9696d1131b55126003ad8888a6457ec99a019
Evans, Morgan <evansm54@uksf.kfplc.com>
2021-07-27 10:06:31 +0100

    sw fix

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,3 +1,4 @@
+/* tslint:disable */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -25,7 +26,7 @@ type Config = {
   onUpdate?: (registration: ServiceWorkerRegistration) => void;
 };
 
-export function register(config?: Config) {
+export function register(config?: Config): void {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -133,7 +134,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
     });
 }
 
-export function unregister() {
+export function unregister(): void {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
       .then((registration) => {
